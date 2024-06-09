b820a4f717ff8c8ee505cfe7e630618889dad6a9
imdaano <danisanchezpsc@gmail.com>
2024-06-09 13:41:20 +0200

    add new Spring Code

---
+++ src/main/java/edu/daniel/valorant/ValorantWorldApplication.java
@@ -0,0 +1,15 @@
+package edu.daniel.valorant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorantWorldApplication.class, args);
+
+		
+	}
+
+}
---
+++ src/main/java/edu/daniel/valorant/entities/Agente.java
@@ -0,0 +1,5 @@
+package edu.daniel.valorant.entities;
+
+public class Agente {
+
+}
