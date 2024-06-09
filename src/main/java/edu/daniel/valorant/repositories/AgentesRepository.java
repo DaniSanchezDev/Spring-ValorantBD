a0d2fd091b78e101dcb05429a33a221fe5a881d9
imdaano <danisanchezpsc@gmail.com>
2024-06-09 13:50:56 +0200

    add new Spring Code

--- src/main/java/edu/daniel/valorant/repositories/AgentesRepository.java
+++ src/main/java/edu/daniel/valorant/repositories/AgentesRepository.java
@@ -1,7 +1,6 @@
 package edu.daniel.valorant.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import edu.daniel.valorant.entities.Agente;
 
 public interface AgentesRepository extends JpaRepository <Agente, Long> {
