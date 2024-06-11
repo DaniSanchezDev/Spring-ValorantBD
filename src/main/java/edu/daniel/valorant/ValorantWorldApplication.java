b6fbcd024b01609885b1909cf9639fa346c24432
imdaano <danisanchezpsc@gmail.com>
2024-06-11 08:47:09 +0200

    add new Spring Code

--- src/main/java/edu/daniel/valorant/ValorantWorldApplication.java
+++ src/main/java/edu/daniel/valorant/ValorantWorldApplication.java
@@ -1,15 +1,28 @@
 package edu.daniel.valorant;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import edu.daniel.valorant.repositories.AgentesRepository;
+
 @SpringBootApplication
-public class ValorantWorldApplication {
+// añadir el implements CommandLineRunner
+public class ValorantWorldApplication implements CommandLineRunner {
+
+	@Autowired 
+	private AgentesRepository agentesRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ValorantWorldApplication.class, args);
 
-		
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Número de agentes almacenador en la tabla: " + agentesRepo.count()) ;
 	}
 
 }
