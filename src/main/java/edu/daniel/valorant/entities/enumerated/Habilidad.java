0afd3b75642d0bce0c4bfe188e59300afdad8d5d
imdaano <danisanchezpsc@gmail.com>
2024-06-15 11:52:48 +0200

    add new Spring Code

--- src/main/java/edu/daniel/valorant/entities/enumerated/Habilidad.java
+++ src/main/java/edu/daniel/valorant/entities/enumerated/Habilidad.java
@@ -1,9 +1,15 @@
 package edu.daniel.valorant.entities.enumerated;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Habilidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
     private Long id;
     private String nombre;
     private String descripcion;
