b0e720a993f9869b4db6dd1abb26d461c160cd5d
imdaano <danisanchezpsc@gmail.com>
2024-06-28 09:04:23 +0200

    add new Spring Code

--- src/main/java/edu/daniel/valorant/ValorantWorldApplication.java
+++ src/main/java/edu/daniel/valorant/ValorantWorldApplication.java
@@ -1,10 +1,15 @@
 package edu.daniel.valorant;
 
+import java.util.List;
+
+import org.aspectj.weaver.loadtime.Agent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import edu.daniel.valorant.entities.Agente;
+import edu.daniel.valorant.entities.enumerated.Rol;
 //import edu.daniel.valorant.entities.enumerated.Rol;
 import edu.daniel.valorant.repositories.AgentesRepository;
 
@@ -27,8 +32,11 @@ public class ValorantWorldApplication implements CommandLineRunner {
 		System.out.println("Número de agentes almacenador en la tabla: " + agentesRepo.count());
 		System.out.println(agentesRepo.findById((long) 20));
 		// agentesRepo.deleteById((long) 20);
-
+		System.out.println(agentesRepo.findByNombreAndPaisAndRol("reyna", "mexico", Rol.DUELISTA));
+		 System.out.println(agentesRepo.findByIdAgenteGreaterThan(15));
 		// System.out.println(agentesRepo.findByRol(Rol.CENTINELA));
+
+		List <Agente> resultsConsults = agentesRepo.findByIdAgenteGreaterThan()
 	}
 
 }
--- src/main/java/edu/daniel/valorant/repositories/AgentesRepository.java
+++ src/main/java/edu/daniel/valorant/repositories/AgentesRepository.java
@@ -11,5 +11,7 @@ public interface AgentesRepository extends JpaRepository <Agente, Long> {
     public List<Agente> findByRol(Rol rol);
     public List<Agente> findByPais(String pais);
     public Agente findByNombreAndPais (String nombre, String pais);
+    public Agente findByNombreAndPaisAndRol(String nombre, String pais, Rol rol);
+    public List<Agente> findByIdAgenteGreaterThan(int idAgente);
 
 }
